cf166b1a955e7113ceab512608aa9c42f2ca538a
Achifa <akpulukelvin@gmail.com>
2023-04-19 13:06:27 +0100

    update urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from .views import index, callback, artists, recently_played, home, tracks_by_term, artists_by_term
+from .views import (
+    index,
+    callback,
+    artists,
+    recently_played,
+    home,
+    tracks_by_term,
+    artists_by_term,
+)
 
 
 app_name = "app"
@@ -9,8 +17,8 @@ urlpatterns = [
     path("", index, name="index"),
     path("callback", callback, name="callback"),
     path("stats/", home, name="home"),
-    path("stats/top-artists/<str:term>/", artists_by_term, name="long_term"),
-    path("stats/top-tracks/<str:term>/", tracks_by_term, name="mid_term"),
-    path("stats/top-artists/", artists, name="short_term"),
+    path("stats/top-artists/<str:term>/", artists_by_term, name="artists_term"),
+    path("stats/top-tracks/<str:term>/", tracks_by_term, name="tracks_term"),
+    path("stats/top-artists/", artists, name="artists"),
     path("stats/recently-played/", recently_played, name="recently_played"),
 ]
